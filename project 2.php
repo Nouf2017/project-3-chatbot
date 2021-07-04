5c1228983aa0be9e3295c75705dd1417d968cb55
Asery. Nouf <85813105+Nouf2017@users.noreply.github.com>
2021-07-04 03:24:01 +0300

    Add files via upload
    هذه المهمه تدور حول الشات بوت وهو برنامج يعتمد ويتخصص في الذكاء الصناعي بحيث يتفاعل مع المستخدم (الانسان )بلغه طبيعيه يمكن استيعابها . لاداء هذه المهمه قمت بانشاء شات بوت يتحدث حول الروبوتات التي يمكن تقديمها من خلال الاساليب الذكيه ونوع الخدمه التي قد تكون ايجار او شراء وانواع الروبوتات . يوجد صوره توضح ذلك

---
+++ project 2.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url("https://cdn.al-ain.com/lg/images/2017/6/15/78-145420-chinese-robots-5.jpeg");
+  background-size:100% 100%;
+  background-attachment:fixed;
+  background-repeat: no-repeat;
+}
+</style>
+</head>
+<body>
+<font size ="6" color="bule" >
+<center>Control Interface Design To Move The Base</center>
+</font>
+<br></br><br></br>
+
+<body>
+<center>
+    <form method="POST" action="re.php">
+    <input type="submit" value="FORWARD" name= "FORWARD" style= "background-color:#CD5C5C; font-size: large;padding: 20px 20px;text-align:center;text-decoration:center;
+display: inline-block;
+    font-size: 15px;
+    margin: 6px 6px;
+    cursor: center ;webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+    webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius:4px;
+  .submit{box-shadow:2px 1px 4px 2px black;background-color: #008CBA;
+ border-radius: 8px; hover background-color:white;
+color:blue;">
+    <br><br><br><br>
+    
+    <input type="submit" value="L"  name= "L" style= "background-color:#CD5C5C; font-size: large;padding: 20px 20px;text-align:center;text-decoration:center;
+display: inline-block;
+    font-size: 15px;
+    margin: 6px 6px;
+    cursor: center ;webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+    webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius:4px;
+  .submit{box-shadow:2px 1px 4px 2px black;background-color: #008CBA;
+ border-radius: 8px; hover background-color:white;
+color:blue;">
+    <input type="submit" value="STOP" name= "STOP"  style= "background-color:#ADD8E6; font-size: large;padding: 20px 20px;text-align:center;text-decoration:center;
+display: inline-block;
+    font-size: 15px;
+    margin: 6px 6px;
+    cursor: center ;webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+    webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius:4px;
+  .submit{box-shadow:2px 1px 4px 2px black;background-color: #008CBA;
+ border-radius: 8px; hover background-color:white;
+color:blue;">
+    <input type="submit" value="R" name= "R" style= "background-color:#CD5C5C; font-size: large;padding: 20px 20px;text-align:center;text-decoration:center;
+display: inline-block;
+    font-size: 15px;
+    margin: 6px 6px;
+    cursor: center ;webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+    webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius:4px;
+  .submit{box-shadow:2px 1px 4px 2px black; background-color: #008CBA;
+ border-radius: 8px; hover background-color:white;
+color:blue;">
+<br> <br><br><br>
+    <input type="submit" value="BACKWARD" name= "BACKWARD" style= "background-color:#CD5C5C; font-size: large;padding: 20px 20px;text-align:center;text-decoration:center;
+display: inline-block;
+    font-size: 15px;
+    margin: 6px 6px;
+    cursor: center ;webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+    webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    border-radius:4px;
+  .submit{box-shadow:2px 1px 4px 2px black;background-color: #008CBA;
+ border-radius: 8px; hover background-color:white;
+color:blue;">
+
+
+</center>
+</form>  
+ 
+ 
+<br>
+<script>
+  window.watsonAssistantChatOptions = {
+      integrationID: "ef5da6fd-4d71-4781-beb7-07c0a98c01e8", // The ID of this integration.
+      region: "eu-de", // The region your integration is hosted in.
+      serviceInstanceID: "22f0c1b7-b006-4329-b1f7-66e336b6818a", // The ID of your service instance.
+      onLoad: function(instance) { instance.render(); }
+    };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+    document.head.appendChild(t);
+  });
+</script>
+</body>
+</html>
